641f1ed2a765047fdd33cfa4ab5e0c39981dd915
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-29 11:19:20 -0300

    Criação da interface DAO de vendedor
    Criação da estrutura DAO de vendedor com os métodos de insert, update, delete, findById e findAll

---
+++ src/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.List;
+
+import model.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
