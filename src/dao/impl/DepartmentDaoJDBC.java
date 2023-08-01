8efd588a5b03fd80b48891808d71a3216a0cf6cb
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-31 21:02:39 -0300

    Insert de Departamento
    Método de inserir um departamento.

---
+++ src/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,75 @@
+package dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import dao.DepartmentDao;
+import db.DB;
+import db.DbException;
+import db.DbIntegrityException;
+import model.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+	
+	private Connection connection;
+
+	public DepartmentDaoJDBC(Connection connection) {
+		this.connection = connection;
+	}
+
+	@Override
+	public void insert(Department obj) {
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = connection.prepareStatement("INSERT INTO department "
+					+ "(Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+			preparedStatement.setString(1, obj.getName());
+			
+			int rowsAffected = preparedStatement.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet resultSet = preparedStatement.getGeneratedKeys();
+				if (resultSet.next()) {
+					int id = resultSet.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(resultSet);
+			} else {
+				throw new DbException("Erro inesperado! Nenhuma linha afetada!");
+			}
+		} catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+		}
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
